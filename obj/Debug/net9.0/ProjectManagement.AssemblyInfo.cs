419722d9d3cdc7e30f6234b6b60c7861800f2520
NguyenH Thai <nghthai0224@gmail.com>
2025-03-02 18:51:42 +0700

    commit temp

--- obj/Debug/net9.0/ProjectManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjectManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7d7cbfd9ba430b1ecb5f1947671ccbd29502d4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c011210de9457f63ed72af3d558fa051abb0ead")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
