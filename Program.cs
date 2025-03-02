f7d7cbfd9ba430b1ecb5f1947671ccbd29502d4c
NguyenH Thai <nghthai0224@gmail.com>
2025-03-02 10:32:16 +0700

    config path https

--- Program.cs
+++ Program.cs
@@ -81,6 +81,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
+
 app.UseCors("AllowAll");
 // app.UseHttpsRedirection();
 app.UseAuthentication();
